7df8d8bee86eb8eed17bbe0ff60dc45cc7b1e8bb
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-05-13 15:57:42 +0000

    search_functionality

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from ast import Import
+from ast import Import, keyword
 from unicodedata import category
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
@@ -7,6 +7,7 @@ from category.models import Category
 from store.models import Product
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 # Create your views here.
 
 def store(request, category_slug=None):
@@ -46,3 +47,16 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }        
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count':product_count,
+    }        
+    return render(request, 'store/store.html', context)
