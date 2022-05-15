61fda40c339bce3ecdb7039fb21e89809bf6d4f3
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-05-15 15:09:37 +0000

    login and logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from email import message
+import email
 from django.shortcuts import redirect, render
 from .models import *
 from accounts.models import Account
 from .forms import RegistrationForm
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
     if request.method == "POST":
@@ -22,14 +25,30 @@ def register(request):
             return redirect('register')
     else:
         form = RegistrationForm()
-        print(form.errors, 'test:::::')
     context = {
             'form': form,
         }
     return render(request, 'accounts/register.html', context)
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        print(email, password)
+
+        user = auth.authenticate(request, email=email, password=password)
+        print(user, 'login:::::::')
+        if user is not None:
+            auth.login(request, user) 
+            # messages.success(request, 'Login Successfully')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login Credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
+
+@login_required(login_url='login')
 def logout(request):
-    return       
+    auth.logout(request)
+    return redirect('login')
