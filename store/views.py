c18270be0322f9e49dd63855e7e5c8755748390f
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-05-19 16:54:37 +0000

    store the rating review

--- store/views.py
+++ store/views.py
@@ -1,10 +1,12 @@
-from ast import Import, keyword
+from django.contrib import messages
 from unicodedata import category
+from django.db import DatabaseError
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from carts.models import CartItem
 from category.models import Category
-from store.models import Product
+from store.forms import ReviewForm
+from store.models import Product, ReviewRating
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
@@ -60,3 +62,27 @@ def search(request):
         'product_count':product_count,
     }        
     return render(request, 'store/store.html', context)
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thankyou! Your review has been updated.')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, 'Thank you! Your review has been updated.')
+                return redirect(url)
+    # return redirect('category')
