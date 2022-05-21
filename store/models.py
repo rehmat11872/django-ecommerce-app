bcc1a85fe1d8499e518a45b5542f0ee88b3fbba5
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-05-21 13:05:49 +0000

    Product Gallery Model With Image Preview

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from itertools import product
 from unicodedata import category
 from django.db import models
@@ -86,3 +87,15 @@ class ReviewRating(models.Model):
         return self.subject
 
 
+class ProductGallery(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/product', max_length=255)
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:
+        verbose_name = 'productgallery'
+        verbose_name_plural = 'product gallery'    
+
+    
