0d3f22db742726dab9fae8b8d1d1ca0288cb0abc
rehmat11872 <raorehmat11@gmail.com>
2024-05-14 15:52:06 +0500

    update settings

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -142,7 +142,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'greatkart/static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
